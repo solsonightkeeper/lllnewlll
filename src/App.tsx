93d02a97774c721cf9045c7bde95800e81391f89
solsonightkeeper <solsonightkeeper@gmail.com>
2023-07-12 14:04:55 +0330

    initial up

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import image from "./mk11.png";
+import {
+  Button,
+  AppBar,
+  Page,
+  Spacer,
+  Main,
+  Card,
+  Container,
+  Theme,
+  GridItem,
+  GridContainer,
+} from "@yakad/ui";
+
+function Header() {
+  return (
+    <AppBar>
+      <h1>React </h1>
+      <Spacer />
+      <Button size="small" variant="filled">
+        send
+      </Button>
+    </AppBar>
+  );
+}
+
+function Product() {
+  return (
+    <Card>
+      <img src={image} style={{ width: "100%", borderRadius: "1.0rem" }} />
+      <h3>Moratl Kombat</h3>
+      <span>1000 $</span>
+      <Button size="small" variant="filled" style={{ margin: "10px" }}>
+        Buy
+      </Button>
+    </Card>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Theme color="pink" mode="dark">
+      <Page>
+        <Header />
+        <Main>
+          <Container maxWidth="xs">
+            <GridContainer>
+              <GridItem xl={3}>
+                <Product />
+              </GridItem>
+              <GridItem xl={3}>
+                <Product />
+              </GridItem>
+              <GridItem xl={3}>
+                <Product />
+              </GridItem>
+              <GridItem xl={3}>
+                <Product />
+              </GridItem>
+              <GridItem xl={6}>
+                <Product />
+              </GridItem>
+              <GridItem xl={6}>
+                <Product />
+              </GridItem>
+              <GridItem xl={3}>
+                <Product />
+              </GridItem>
+              <GridItem xl={3}>
+                <Product />
+              </GridItem>
+            </GridContainer>
+          </Container>
+        </Main>
+      </Page>
+    </Theme>
   );
 }
 
